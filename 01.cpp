6d21617eb244f1f77113166774d2f74218098fb8
xybayue-tbk <xybayue@gmail.com>
2026-02-23 11:28:34 +0700

    02

---
+++ 01.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //file yang disertakan
+using namespace std;
+
+int main() // program utama
+
+{
+// menampilkan string
+cout << "selamat datang di TI UMY" << endl ; // menampilkan numeris
+cout << 10;
+}
