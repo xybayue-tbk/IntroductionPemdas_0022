0022a2312991d579ecfbba29018c7bec5107fd40
xybayue-tbk <xybayue@gmail.com>
2026-02-23 11:20:20 +0700

    menambahkan library

--- app.cpp
+++ app.cpp
@@ -1,1 +1,10 @@
-#include <iostream>
+#include <iostream> //file yang disertakan
+using namespace std;
+
+int main() // program utama
+
+{
+// menampilkan string
+cout << "selamat datang di TI UMY" << endl ; // menampilkan numeris
+cout << 10;
+}
