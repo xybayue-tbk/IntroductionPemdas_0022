d088a709323aa6895b55bfe29c7a222cb8235df8
xybayue-tbk <xybayue@gmail.com>
2026-02-23 11:57:53 +0700

    selesai

---
+++ 03.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //langkah1
+    int luas, panjang, lebar ;
+
+    //langkah2
+    cout << "masukan panjang :";
+    cin >> panjang;
+
+    //langkah3
+    cout << "masukan lebar ;";
+    cin >> lebar;
+
+    //langkah4
+    luas = panjang * lebar;
+
+    //langkah5
+    cout << "hasil : " << luas; 
+
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //langkah1
+    int luas, panjang, lebar;
+
+    //langkah2
